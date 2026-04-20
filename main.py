b6070c033b869e43c0d5d96758d0644289384e20
Tyler Chapp <tjc234@nau.edu>
2026-04-20 09:14:30 -0700

    base cnn implement

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 # =========================
 
 # import dependencies
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from medmnist import PathMNIST
-from torchvision import transforms
+from torchvision import transforms, models
 from torch.utils.data import DataLoader
 
 # handle random seed generation
@@ -51,7 +54,25 @@ def create_loaders( train_data, val_data, test_data ):
 
 # simple CNN
 def build_simple_cnn():
-    return None
+
+    class SimpleCNN( nn.Module ):
+        def __init__( self ):
+            super().__init__()
+            self.conv1 = nn.Conv2d( 1, 16, 3, padding = 1 )
+            self.conv2 = nn.Conv2d( 16, 32, 3, padding = 1 )
+            self.fc1 = nn.Linear( 32 * 7 * 7, 128 )
+            self.fc2 = nn.Linear( 128, 9 )  # MedMNIST has 9 tissue classes
+
+        def forward(self, x):
+            x = F.relu( self.conv1( x ) )
+            x = F.max_pool2d (x, 2 )
+            x = F.relu( self.conv2( x ))
+            x = F.max_pool2d( x, 2 )
+            x = x.view(x.size( 0 ), -1 )
+            x = F.relu(self.fc1( x ) )
+            return self.fc2( x )
+
+    return SimpleCNN()
 
 # resnet18
 def build_resnet18( pretrained = False ):
@@ -59,6 +80,10 @@ def build_resnet18( pretrained = False ):
 
 # resnet50
 def build_resnet50( pretrained = False ):
+    return None
+
+# resnet101
+def build_resnet101( pretrained = False ):
     return None
 
 
